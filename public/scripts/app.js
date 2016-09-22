ff11f46079b5051e4c046808b122f72e21242e38
Sammy Tam <sammytam2005@hotmail.com>
2016-09-22 02:31:51 +0000

    Fetching tweets with Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,50 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
- var tweetData = {
-   "user": {
-     "name": "Newton",
-     "avatars": {
-       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-     },
-     "handle": "@SirIsaac"
-   },
-   "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-   "created_at": 1461116232227
- }
+$(document).ready(function(){
 
-function createTweetElement(tweet) {
-  $(document).ready(function(){
 
-    const $input = $(".new-tweet").find("input");
-    $input.click(function (e) {
-      e.preventDefault();
+  function createTweetElement(tweet) {
+    $tweet = $("<article>").addClass("tweet");
 
-      $tweet = $("<article>").addClass("tweet");
+    $tweetHeader = $("<header>").html('<img src="' + tweet.user.avatars.small + '"><h2>' + tweet.user.name + '</h2>');
+    $tweetHandle = $("<span>").addClass("handle").html(tweet.user.handle).appendTo($tweetHeader);
 
-      $tweetHeader = $("<header>").html('<img src="' + tweet.user.avatars.small + '"><h2>' + tweet.user.name + '</h2>');
-      $tweetHandle = $("<span>").addClass("handle").html(tweet.user.handle).appendTo($tweetHeader);
+    $tweetContent = $("<section>").addClass("text").html(tweet.content.text);
 
-      $tweetContent = $("<section>").addClass("text").html(tweet.content.text);
+    var today = Date.now();
+    var daysAgo = Math.round((today - tweet.created_at) / (1000*60*60*24));
+    $tweetFooter = $("<footer>").html(daysAgo + ' days ago');
+    $footerIcons = $("<span>").appendTo($tweetFooter);
 
-      var today = Date.now();
-      var daysAgo = Math.round((today - tweet.created_at) / (1000*60*60*24));
-      $tweetFooter = $("<footer>").html(daysAgo + ' days ago');
-      $footerIcons = $("<span>").appendTo($tweetFooter);
+    $tweet.append($tweetHeader, $tweetContent, $tweetFooter);
 
-      $('.container').append($tweet);
-      $tweet.append($tweetHeader, $tweetContent, $tweetFooter);
+    return $tweet;
+  }
 
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweetData) {
+      var $tweet = createTweetElement(tweetData);
+      $('#tweet-container').append($tweet);
+    });
+  }
+
+  $('.new-tweet').find('form').on('submit', function(Event) {
+    Event.preventDefault();
+    var url = this.action + '?' + $(this).serialize();
+    $.ajax({
+      url: url,
+      method: 'POST',
+      success: function (tweet) {}
     });
   });
-}
 
+  (function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: function(tweets) {
+        renderTweets(tweets);
+      }
+    });
+  }());
 
- var $tweet = createTweetElement(tweetData);
-
- // Test / driver code (temporary)
- console.log($tweet); // to see what it looks like
+});
--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
 
-  $('#tweetContainer').on('mouseenter', '.tweet', function (event) {
+  $('#tweet-container').on('mouseenter', '.tweet', function (event) {
     let $this = $(this);
     $this.children('header').addClass('hover');
     $this.children('footer').find('span').addClass('widget');
   });
 
-  $('#tweetContainer').on('mouseleave', '.tweet', function (event) {
+  $('#tweet-container').on('mouseleave', '.tweet', function (event) {
     let $this = $(this);
     $this.children('header').removeClass('hover');
     $this.children('footer').find('span').removeClass('widget');
